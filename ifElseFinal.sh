099866ddc2ff08b86926d9f740b5196daab05ef6
ZafarAabidBL <32291348+ZafarAabid@users.noreply.github.com>
2019-11-09 17:48:01 +0530

    refactoring my code by creating conflict

--- ifElseFinal.sh
+++ ifElseFinal.sh
@@ -5,11 +5,11 @@ present=$(( RANDOM % 3))
 
 if [ $isPresent < $present ]
 then
-	echo Person is absent
+	echo Person is absent ???
 elif [ $isPresent -eq $present ]
-	echo person is absent
+	echo person is absent.....
 else 
-	echo Person is flee
+	echo Person is flee !!!!
 done
 
 
