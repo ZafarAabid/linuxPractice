82a6e238709c96bf872785ef2ed53e715121ec20
Akshayrp <akshaypatwari24@gmail.com>
2019-11-09 17:35:33 +0530

    Edited

--- ifElseFinal.sh
+++ ifElseFinal.sh
@@ -5,11 +5,13 @@ present=$(( RANDOM % 3))
 
 if [ $isPresent < $present ]
 then
+
+
 	echo Person is absent
 elif [ $isPresent -eq $present ]
 	echo person is absent
 else 
-	echo Person is flee
+	echo Person is fled
 done
 
 
